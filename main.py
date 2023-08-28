c28318cb8474f127db79a89c56a29dee800cbd6a
Shayaan <khanshayaan1@gmail.com>
2023-08-28 18:17:56 +0530

    added a copy pasta submission command and its data

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from discord.ext import commands, tasks
 import requests
 from dotenv import load_dotenv
 import os
+import json
+import asyncio
+
 
 load_dotenv()
 token = os.getenv("TOKEN")
+
+# Load keyword-text pairs from JSON file
+def load_responses():
+    try:
+        with open('responses.json', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return []
+
+responses = load_responses()
+
 
 intents = discord.Intents.all()
 intents.typing = False
@@ -20,17 +34,68 @@ platform = "pc"
 async def on_ready():
   print("Logged in as a bot {0.user}".format(bot))
 
+
+# Event listener that pastes copypastas
 @bot.event
 async def on_message(message):
+    global responses
+
     if message.author.bot:
         return
-
-    if 'crazy' in message.content.lower():
-        response = "Crazy? \nI was crazy once. \nThey locked me in a room. \nA rubber room. \nA rubber room with rats. \nThe rats made me crazy. "
-        await message.channel.send(response)
+    
+    for response in responses:
+        keyword = response['keyword']
+        text = response['text']
+        if keyword.lower() in message.content.lower():
+            await message.channel.send(text)
+            break  # Exit loop after the first match
 
     await bot.process_commands(message)
 
+
+
+# Command to submit custom keyword-text pair
+@bot.command()
+async def submit(ctx):
+    global responses  # Declare as global to access outside the function
+
+    def check_author(m):
+        return m.author == ctx.author and m.channel == ctx.channel
+
+    await ctx.send("Please enter the keyword:")
+    keyword_msg = await bot.wait_for('message', check=check_author)
+    keyword_input = keyword_msg.content
+    
+    # Cancels the function    
+    if keyword_input.lower() == 'cancel':
+        await ctx.send("Submission canceled.")
+        return
+
+    await ctx.send("Please enter the text:")
+    text_msg = await bot.wait_for('message', check=check_author)
+    text_input = text_msg.content
+
+    # Cancels the function
+    if text_input.lower() == 'cancel':
+        await ctx.send("Submission canceled.")
+        return
+
+    keyword = keyword_msg.content
+    text = text_msg.content
+    user_id = ctx.author.id
+
+    # Add new keyword-text pair to responses list
+    new_response = {'keyword': keyword, 'text': text, 'user_id': user_id}
+    responses.append(new_response)
+
+    # Write responses list to JSON file
+    with open('responses.json', 'w') as file:
+        json.dump(responses, file, indent=4)
+
+    await ctx.send(f"Keyword '{keyword}' with text '{text}' has been submitted by user {ctx.author.mention}.")
+
+
+# Command to show day night info for the plains of eidolon
 @bot.command()
 async def cetus(ctx):
   # Make an API request to get Cetus cycle data
