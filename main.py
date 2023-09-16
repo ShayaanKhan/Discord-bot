752cc8f29717521d75442c683b1c13a047a20ff5
Shayaan <khanshayaan1@gmail.com>
2023-09-16 16:53:12 +0530

    Code refactoring

--- main.py
+++ main.py
@@ -1,181 +1,123 @@
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 import requests
 from dotenv import load_dotenv
 import os
 import json
-import asyncio
 import time
+from typing import List, Dict, Any
 
-
+# Load environment variables from .env file
 load_dotenv()
-token = os.getenv("TOKEN")
 
-# Load keyword-text pairs from JSON file
-def load_responses():
+# Retrieve token from environment variable
+token: str = os.getenv("TOKEN")
+
+# Create Discord intents object with all intents enabled except typing and presences
+intents = discord.Intents.all()
+intents.typing = False
+intents.presences = False
+intents.messages = True
+
+# Create a bot instance with the specified command prefix and intents
+bot: commands.Bot = commands.Bot(command_prefix="!", intents=intents)
+
+# Set the platform to "pc"
+platform: str = "pc"
+
+# List to store responses loaded from responses.json
+responses: List[Dict[str, str]] = []
+
+# Dictionary to store cooldowns for each keyword and user combination
+keyword_cooldowns: Dict[str, float] = {}
+
+# Base URL for Warframe API
+WARFRAME_API_BASE_URL: str = "https://api.warframe.com/"
+
+# Function to load responses from responses.json file
+def load_responses() -> List[Dict[str, str]]:
     try:
         with open('responses.json', 'r') as file:
             return json.load(file)
     except FileNotFoundError:
         return []
 
-responses = load_responses()
-
-
-intents = discord.Intents.all()
-intents.typing = False
-intents.presences = False
-intents.messages = True
-
-bot = commands.Bot(command_prefix="!", intents=intents)
-platform = "pc"
-
-keyword_cooldowns = {}
-
-
-def write_responses():
+# Function to write responses to responses.json file
+def write_responses() -> None:
     with open('responses.json', 'w') as file:
         json.dump(responses, file, indent=4)
 
-
-# Events
-
+# Event handler for when the bot is ready
 @bot.event
-async def on_ready():
-  print("Logged in as a bot {0.user}".format(bot))
+async def on_ready() -> None:
+    print("Logged in as a bot {0.user}".format(bot))
 
-
-# Event listener that pastes copypastas
+# Event handler for when a message is received
 @bot.event
-async def on_message(message):
+async def on_message(message: discord.Message) -> None:
+    # Ignore messages sent by bots
     if message.author.bot:
         return
 
-    # Check if any keyword matches in the message
+    # Iterate through each response
     for response in responses:
-        keyword = response['keyword']
-        text = response['text']
-
-        # Check if the keyword is on cooldown for this user
-        cooldown_key = f'{message.author.id}-{keyword.lower()}'
-        current_time = time.time()
-
+        # Extract keyword and text from response
+        keyword: str = response['keyword']
+        text: str = response['text']
+        
+        # Generate a unique cooldown key for each user and keyword combination
+        cooldown_key: str = f'{message.author.id}-{keyword.lower()}'
+        
+        # Get the current time
+        current_time: float = time.time()
+        
+        # Check if the user is on cooldown for the keyword
         if cooldown_key in keyword_cooldowns and current_time - keyword_cooldowns[cooldown_key] < 120:
             return
-
+        
+        # Check if the keyword is present in the message content
         if keyword.lower() in message.content.lower():
+            # Send the response text to the channel
             await message.channel.send(text)
+            
+            # Update the cooldown for the user and keyword
             keyword_cooldowns[cooldown_key] = current_time
-            break  # Exit loop after the first match
+            
+            # Exit the loop after sending the response
+            break
 
+    # Process the message as a command
     await bot.process_commands(message)
 
+# Command to get the current Cetus cycle in Warframe
 
-# Commands
-
-# Command to show day night info for the plains of eidolon
 @bot.command()
 async def cetus(ctx):
-  # Make an API request to get Cetus cycle data
-  response = requests.get(f"https://api.warframestat.us/{platform}/cetusCycle")
-  data = response.json()
+    """
+    Retrieves the Cetus cycle data from the Warframe API and sends a message with the current state and time remaining.
 
-  state = data["state"]
-  time_left = data["timeLeft"]
+    Args:
+        ctx (object): The context object representing the invocation context of the command.
 
-  if state == "day":
-    state_text = "Daytime"
-  else:
-    state_text = "Nighttime"
+    Returns:
+        None
+    """
+    # Make a GET request to the Warframe API to retrieve Cetus cycle data
+    response = requests.get(f"https://api.warframestat.us/{platform}/cetusCycle")
+    data = response.json()
 
-  message = f"The current Cetus cycle is in {state_text}. Time remaining: {time_left}"
-  await ctx.send(message)
+    # Extract the state and time left from the API response
+    state = data["state"]
+    time_left = data["timeLeft"]
 
-# Warframe API base URL
-WARFRAME_API_BASE_URL = 'https://api.warframe.market/v1/'
+    # Map the state to a human-readable text
+    if state == "day":
+        state_text = "Daytime"
+    else:
+        state_text = "Nighttime"
 
-@bot.command()
-async def godrolls(ctx, weapon_name):
-    # Fetch weapon information from Warframe API
-    weapon_url = f'{WARFRAME_API_BASE_URL}weapons/search?q={weapon_name}'
-    response = requests.get(weapon_url)
-    weapon_data = response.json()
-
-    if not weapon_data.get('payload', {}).get('items'):
-        await ctx.send("Weapon not found.")
-        return
-
-    weapon_id = weapon_data['payload']['items'][0]['id']
-
-    # Fetch Riven mod data for the weapon
-    riven_url = f'{WARFRAME_API_BASE_URL}rivens/search/{weapon_id}'
-    response = requests.get(riven_url)
-    riven_data = response.json()
-
-    if not riven_data.get('payload', {}).get('riven_mods'):
-        await ctx.send("No Riven mods found for this weapon.")
-        return
-
-    godrolls = []
-
-    # Define your criteria for godrolls (example: high positive stats)
-    for riven_mod in riven_data['payload']['riven_mods']:
-        positive_stats = [stat for stat in riven_mod['rerolled']['positive_stats'] if stat['value'] > 1.0]
-        if len(positive_stats) >= 3:
-            godrolls.append(riven_mod)
-
-    if not godrolls:
-        await ctx.send("No godroll Riven mods found for this weapon.")
-        return
-
-    # Display the godrolls
-    for godroll in godrolls:
-        stats_str = ', '.join([f"{stat['value']} {stat['name']}" for stat in godroll['rerolled']['positive_stats']])
-        message = f"Godroll Riven: {godroll['item_name']} with stats {stats_str}"
-        await ctx.send(message)
-
-
-# Command to submit custom keyword-text pair
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def submitpasta(ctx):
-    global responses  # Declare as global to access outside the function
-
-    def check_author(m):
-        return m.author == ctx.author and m.channel == ctx.channel
-
-    await ctx.send("Please enter the keyword:")
-    keyword_msg = await bot.wait_for('message', check=check_author)
-    keyword_input = keyword_msg.content
+    # Format the message with the Cetus cycle information
+    message = f"The current Cetus cycle is in {state_text}. Time remaining: {time_left}"
     
-    # Cancels the function    
-    if keyword_input.lower() == 'cancel':
-        await ctx.send("Submission canceled.")
-        return
-
-    await ctx.send("Please enter the text:")
-    text_msg = await bot.wait_for('message', check=check_author)
-    text_input = text_msg.content
-
-    # Cancels the function
-    if text_input.lower() == 'cancel':
-        await ctx.send("Submission canceled.")
-        return
-
-    keyword = keyword_msg.content
-    text = text_msg.content
-    user_id = ctx.author.id
-
-    # Add new keyword-text pair to responses list
-    new_response = {'keyword': keyword, 'text': text, 'user_id': user_id}
-    responses.append(new_response)
-
-    # Write responses list to JSON file
-    with open('responses.json', 'w') as file:
-        json.dump(responses, file, indent=4)
-
-    await ctx.send(f"Keyword '{keyword}' with text '{text}' has been submitted by user {ctx.author.mention}.")
-
-
-
-bot.run(token)
+    # Send the message to the channel
+    await ctx.send(message)
