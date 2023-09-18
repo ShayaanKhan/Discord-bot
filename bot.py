e596227db3f5394d42039548cbbc2c6efc8ae0b1
Shayaan <khanshayaan1@gmail.com>
2023-09-18 10:13:41 +0530

    minor changes

--- bot.py
+++ bot.py
@@ -34,62 +34,63 @@ keyword_cooldowns: Dict[str, float] = {}
 # Base URL for Warframe API
 WARFRAME_API_BASE_URL: str = "https://api.warframe.com/"
 
+# create a json file to store chat messages
+if not os.path.exists("texts.json"):
+    with open("texts.json", "w") as file:
+        json.dump([], file)
+
+
 # Function to load responses from responses.json file
 def load_responses() -> List[Dict[str, str]]:
     try:
-        with open('responses.json', 'r') as file:
+        with open("responses.json", "r") as file:
             return json.load(file)
     except FileNotFoundError:
         return []
 
+
 # Function to write responses to responses.json file
 def write_responses() -> None:
-    with open('responses.json', 'w') as file:
+    with open("responses.json", "w") as file:
         json.dump(responses, file, indent=4)
+
 
 # Event handler for when the bot is ready
 @bot.event
 async def on_ready() -> None:
     print("Logged in as a bot {0.user}".format(bot))
 
-# Event handler for when a message is received
+
+# event to read chat messages and store them in a texts.json file containing the text as well as user ID.
 @bot.event
-async def on_message(message: discord.Message) -> None:
-    # Ignore messages sent by bots
-    if message.author.bot:
+async def on_message(message):
+    """
+    This function is an event handler that is triggered whenever a message is received.
+    It takes in a `message` parameter, which represents the message object.
+    The function does the following:
+    1. Checks if the message is not from the bot. If it is, the function returns.
+    2. If the message is from someone else, it stores the message content and the user ID in a JSON file.
+    3. It then checks if the message is a command. If it is, the function returns.
+    4. Finally, it processes the commands in the message.
+    """
+    # if the message is not from the bot, ignore it
+    if message.author == bot.user:
         return
+    # if the message is from someone else, store it in the json file
+    with open("texts.json", "r") as file:
+        texts = json.load(file)
+    texts.append({"text": message.content, "user_id": message.author.id})
+    with open("texts.json", "w") as file:
+        json.dump(texts, file, indent=4)
 
-    # Iterate through each response
-    for response in responses:
-        # Extract keyword and text from response
-        keyword: str = response['keyword']
-        text: str = response['text']
-        
-        # Generate a unique cooldown key for each user and keyword combination
-        cooldown_key: str = f'{message.author.id}-{keyword.lower()}'
-        
-        # Get the current time
-        current_time: float = time.time()
-        
-        # Check if the user is on cooldown for the keyword
-        if cooldown_key in keyword_cooldowns and current_time - keyword_cooldowns[cooldown_key] < 120:
-            return
-        
-        # Check if the keyword is present in the message content
-        if keyword.lower() in message.content.lower():
-            # Send the response text to the channel
-            await message.channel.send(text)
-            
-            # Update the cooldown for the user and keyword
-            keyword_cooldowns[cooldown_key] = current_time
-            
-            # Exit the loop after sending the response
-            break
-
-    # Process the message as a command
+    # if the message is a command, process it
+    if message.content.startswith("!"):
+        return
     await bot.process_commands(message)
 
+
 # Command to get the current Cetus cycle in Warframe
+
 
 @bot.command()
 async def cetus(ctx):
@@ -118,6 +119,16 @@ async def cetus(ctx):
 
     # Format the message with the Cetus cycle information
     message = f"The current Cetus cycle is in {state_text}. Time remaining: {time_left}"
-    
+
     # Send the message to the channel
     await ctx.send(message)
+
+#funtion to show the responses
+@bot.command
+async def show(ctx):
+    await ctx.send(responses)
+    
+
+
+# Run the bot
+bot.run(token)
